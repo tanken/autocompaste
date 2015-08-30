5e5b3752d5e98af35fd1c40196e710dab27e1b12
tanken <tan.kenson@gmail.com>
2015-08-31 00:31:28 +0800

    Add dialog + timer

---
+++ js/init.js
@@ -0,0 +1,49 @@
+/*$(document).ready(function() {
+
+$(document).mousemove(function(e) {
+
+if(e.pageY <= 5)
+{
+
+// Launch MODAL BOX
+$('#exit_content').modal({onOpen: modalOpen, onClose: simplemodal_close});
+}
+
+});
+
+});*/
+
+/**
+ * When the open event is called, this function will be used to 'open'
+ * the overlay, container and data portions of the modal dialog.
+ *
+ * onOpen callbacks need to handle 'opening' the overlay, container
+ * and data.
+ */
+function modalOpen (dialog) {
+	dialog.overlay.fadeIn('fast', function () {
+		dialog.container.fadeIn('fast', function () {
+			dialog.data.hide().slideDown('fast');
+		});
+	});
+}
+
+   /**
+ * When the close event is called, this function will be used to 'close'
+ * the overlay, container and data portions of the modal dialog.
+ *
+ * The SimpleModal close function will still perform some actions that
+ * don't need to be handled here.
+ *
+ * onClose callbacks need to handle 'closing' the overlay, container
+ * and data.
+ */
+function simplemodal_close (dialog) {
+	dialog.data.fadeOut('fast', function () {
+		dialog.container.hide('fast', function () {
+			dialog.overlay.slideUp('fast', function () {
+				$.modal.close();
+			});
+		});
+	});
+}
