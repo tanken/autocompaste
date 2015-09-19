c213b1ceb93424e05a9eb6c4b909d793e4fca59d
tanken <tan.kenson@gmail.com>
2015-09-19 22:20:36 +0800

    Extend columns

--- js/autocompaste/interface.js
+++ js/autocompaste/interface.js
@@ -122,7 +122,7 @@ AutoComPaste.Interface = (function () {
                             .addClass('autocompaste-textarea')
                             .attr({
                               rows: 10,
-                              cols: 46        // Set Text Editor Size
+                              cols: 60        // Set Text Editor Size
                             });
 
         //  For ACP mode, engine is passed into the interface. 
@@ -140,7 +140,7 @@ AutoComPaste.Interface = (function () {
         privates.wm.createWindow("text_editor");
         privates.wm.setWindowTitle("text_editor", "Text Editor");
         privates.wm.setWindowContent('text_editor', acp_textarea);
-        privates.wm.setWindowPrompter("text_editor", "After entering Source 1, press <b>Esc</b>+<b>Enter</b> before entering Source 2.");
+        privates.wm.setWindowPrompter("text_editor", "After entering Source 1, hit <b>Esc</b>+<b>Enter</b> before entering Source 2.");
         acp_textarea.focus();
 
         // Dispatch an event.
