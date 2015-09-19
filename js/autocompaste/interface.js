ce19c818f22e9937aa9218650dec98d9175a38ed
tanken <tan.kenson@gmail.com>
2015-09-19 21:57:05 +0800

    Prompter

--- js/autocompaste/interface.js
+++ js/autocompaste/interface.js
@@ -140,7 +140,7 @@ AutoComPaste.Interface = (function () {
         privates.wm.createWindow("text_editor");
         privates.wm.setWindowTitle("text_editor", "Text Editor La");
         privates.wm.setWindowContent('text_editor', acp_textarea);
-        privates.wm.setWindowFooter("text_editor", "Hit <b>Esc</b> to exit <i>AutoComPaste mode</i>.");
+        privates.wm.setWindowPrompter("text_editor", "Hit <b>Esc</b> to exit <i>AutoComPaste mode</i>.");
         acp_textarea.focus();
 
         // Dispatch an event.
